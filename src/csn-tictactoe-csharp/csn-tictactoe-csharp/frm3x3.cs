78abd4f4cd3aa8a6b0ae61ea479ae5394f664e8a
khaivinhtvu <149222638+khaivinhtvu@users.noreply.github.com>
2023-12-10 16:05:11 +0700

    Add files via upload

--- src/csn-tictactoe-csharp/csn-tictactoe-csharp/frm3x3.cs
+++ src/csn-tictactoe-csharp/csn-tictactoe-csharp/frm3x3.cs
@@ -15,15 +15,58 @@ namespace csn_tictactoe_csharp
         public frm3x3()
         {
             InitializeComponent();
+            khoitao();
         }
 
         private string kytunguoichoi;
-        private int[,] kt = new int[3,3];
+        Button[,] btns = new Button[3, 3];
 
         public string Kytunguoichoi3
         {
             get { return kytunguoichoi; }
             set { kytunguoichoi = value; }
+        }
+
+        private void khoitao()
+        {
+            btns[0, 0] = btn1;
+            btns[0, 1] = btn2;
+            btns[0, 2] = btn3;
+            btns[1, 0] = btn4;
+            btns[1, 1] = btn5;
+            btns[1, 2] = btn6;
+            btns[2, 0] = btn7;
+            btns[2, 1] = btn8;
+            btns[2, 2] = btn9;
+        }
+
+        private void datlainut()
+        {
+            btn1.Text = "";
+            btn2.Text = "";
+            btn3.Text = "";
+            btn4.Text = "";
+            btn5.Text = "";
+            btn6.Text = "";
+            btn7.Text = "";
+            btn8.Text = "";
+            btn9.Text = "";
+        }
+
+        private void Thang(int x, int y, int n, string chu, Button[,] b)
+        {
+            int dk;
+            dk = dieukieuthang.Thang(x, y, n, chu, b);
+            if (dk == 1 && chu == "X")
+            {
+                MessageBox.Show("X thắng!");
+                datlainut();
+            }
+            if (dk == 1 && chu == "O")
+            {
+                MessageBox.Show("O thắng!");
+                datlainut();
+            }
         }
 
         private void frm3x3_Activated(object sender, EventArgs e)
@@ -45,11 +88,6 @@ namespace csn_tictactoe_csharp
             }
         }
 
-        private void Thang()
-        {
-            
-        }
-
         private void btn1_Click(object sender, EventArgs e)
         {
             switch (kytunguoichoi)
@@ -68,7 +106,8 @@ namespace csn_tictactoe_csharp
                     }
             }
 
-            Thang();
+            //Thang(btn1.Text);
+            Thang(0, 0, 3, btn1.Text, btns);
         }
 
         private void btn2_Click(object sender, EventArgs e)
@@ -89,7 +128,8 @@ namespace csn_tictactoe_csharp
                     }
             }
 
-            Thang();
+            //Thang(btn2.Text);
+            Thang(0, 1, 3, btn2.Text, btns);
         }
 
         private void btn3_Click(object sender, EventArgs e)
@@ -110,7 +150,8 @@ namespace csn_tictactoe_csharp
                     }
             }
 
-            Thang();
+            //Thang(btn3.Text);
+            Thang(0, 2, 3, btn3.Text, btns);
         }
 
         private void btn4_Click(object sender, EventArgs e)
@@ -130,6 +171,9 @@ namespace csn_tictactoe_csharp
                         break;
                     }
             }
+
+            //Thang(btn4.Text);
+            Thang(1, 0, 3, btn4.Text, btns);
         }
 
         private void btn5_Click(object sender, EventArgs e)
@@ -149,6 +193,9 @@ namespace csn_tictactoe_csharp
                         break;
                     }
             }
+
+            //Thang(btn5.Text);
+            Thang(1, 1, 3, btn5.Text, btns);
         }
 
         private void btn6_Click(object sender, EventArgs e)
@@ -168,6 +215,9 @@ namespace csn_tictactoe_csharp
                         break;
                     }
             }
+
+            //Thang(btn6.Text);
+            Thang(1, 2, 3, btn6.Text, btns);
         }
 
         private void btn7_Click(object sender, EventArgs e)
@@ -187,6 +237,9 @@ namespace csn_tictactoe_csharp
                         break;
                     }
             }
+
+            //Thang(btn7.Text);
+            Thang(2, 0, 3, btn7.Text, btns);
         }
 
         private void btn8_Click(object sender, EventArgs e)
@@ -206,6 +259,9 @@ namespace csn_tictactoe_csharp
                         break;
                     }
             }
+
+            //Thang(btn8.Text);
+            Thang(2, 1, 3, btn8.Text, btns);
         }
 
         private void btn9_Click(object sender, EventArgs e)
@@ -225,6 +281,9 @@ namespace csn_tictactoe_csharp
                         break;
                     }
             }
+
+            //Thang(btn9.Text);
+            Thang(2, 2, 3, btn9.Text, btns);
         }
     }
 }
